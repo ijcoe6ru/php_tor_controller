4f89cae7275cb8f75825477700cedb3603845496
ijcoe6ru <s6yg1ez3@mail2tor.com>
2016-04-07 03:07:09 +0000

    some code rewritten, less resource usage

--- config.php
+++ config.php
@@ -1,1 +1,1 @@
-<?php $require_login=0;$tc_connection_method=1;$tc_connection_hostname='127.0.0.1';$tc_connection_auth=1;$tc_connection_password='';$tc_connection_secure=0;$tc_connection_port=9051;
+<?php $require_login=0;$tc_connection_method=1;$tc_connection_hostname='127.0.0.1';$tc_connection_auth=1;$tc_connection_password='eWpNgKCaL0bH25vp0Qnasf2IWpqVOs51+61bcxWDEuoA6ksH6GWJ85WAJI2arHKqaRl+spBnGQKBFPAfo3UwC4iG';$tc_connection_secure=0;$tc_connection_port=9051;
